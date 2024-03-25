b46e1276c1d987740ad66d6131108e35473b6865
EmilianaManzo <emiliana.ma96@gmail.com>
2024-03-25 14:50:43 +0100

    Scaffolding

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+  import axios from 'axios';
+  import Header from './components/Header.vue';
+  import Main from './components/Main.vue';
+  export default {
+    components:{
+      Header,
+      Main
+    }
+  }
+</script>
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
